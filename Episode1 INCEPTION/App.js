7bc771f2b9c2f3cabeabe950428c67dc6389ef14
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-06-11 11:59:03 +0530

    episode1

---
+++ Episode1 INCEPTION/App.js
@@ -0,0 +1,11 @@
+const heading=React.createElement("h1",{id:"heading"},"Hello World from React");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{id:"heading"},"Hello from nested divs"),("h2",{id:"heading2"},"Hello from nested divs")]));
+
+const root1=ReactDOM.createRoot(document.getElementById("root1"));
+console.log(root1);
+root1.render(parent);
